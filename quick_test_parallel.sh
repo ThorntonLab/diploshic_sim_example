cacc3de97667c9ed7f7f2bbf9d408cd39f18b976
molpopgen <krthornt@uci.edu>
2018-08-26 09:49:13 -0700

    use priors and fix swap of hard/soft training data

--- quick_test_parallel.sh
+++ quick_test_parallel.sh
@@ -2,7 +2,7 @@
 # Super-quick haploid example
 
 # Neutral training data
-mspms 50 50 -t 1000 -r 1000 10000 > neutral_data_msprime.txt
+mspms 50 500 -t 1000 -r 1000 10000 > neutral_data_msprime.txt
 
 
 # You have to make and maintain all the directories yourself:
@@ -33,8 +33,8 @@ fi
 for x in $(seq 0 0.1 1)
 do
     # Make selected data
-    echo "~/src/discoal/discoal 50 50 1000 -ws 0 -a 1000 -x $x -t 1000 -r 1000 > hard_$WIN.discoal" >> simcommands.txt
-    echo "~/src/discoal/discoal 50 50 1000 -ws 0 -a 1000 -x $x -f 0.1 -t 1000 -r 1000 > soft_$WIN.discoal" >> simcommands.txt
+    echo "~/src/discoal/discoal 50 500 1000 -ws 0 -Pa 1000 2500 -x $x -t 1000 -r 1000 > hard_$WIN.discoal" >> simcommands.txt
+    echo "~/src/discoal/discoal 50 500 1000 -ws 0 -Pa 1000 2500 -x $x -Pf 0.1 0.5 -t 1000 -r 1000 > soft_$WIN.discoal" >> simcommands.txt
     #The file names cannot have multiple _ in them
     echo "python3 diploSHIC.py fvecSim haploid hard_$WIN.discoal hardTraining/hard_$WIN.txt" >> fveccommands.txt
     echo "python3 diploSHIC.py fvecSim haploid soft_$WIN.discoal softTraining/soft_$WIN.txt" >> fveccommands.txt
@@ -44,8 +44,8 @@ done
 # Make the features from neutral data
 echo "python3 diploSHIC.py fvecSim haploid neutral_data_msprime.txt neutral_data_msprime_features.txt" >> fveccommands.txt
 
-parallel --jobs 8 < simcommands.txt
-parallel --jobs 8  < fveccommands.txt
+parallel --jobs 40 < simcommands.txt
+parallel --jobs 40  < fveccommands.txt
 
 
 if [ ! -d training_out ]
@@ -55,7 +55,7 @@ else
     rm -f training_out/*
 fi
 
-python3 diploSHIC.py makeTrainingSets neutral_data_msprime_features.txt hardTraining/hard softTraining/soft 5 0,1,2,3,4,6,7,8,9,10 training_out
+python3 diploSHIC.py makeTrainingSets neutral_data_msprime_features.txt softTraining/soft hardTraining/hard 5 0,1,2,3,4,6,7,8,9,10 training_out
     
 python3 diploSHIC.py train training_out/ training_out/ modelfile.txt
 
